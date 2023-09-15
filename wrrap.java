326ebdd69e217973c18291d17482674585dc1e7f
Vivek Kumar Singh <97895307+vivekkumarsing@users.noreply.github.com>
2023-09-15 15:09:11 +0530

    Add files via upload

---
+++ wrrap.java
@@ -0,0 +1,10 @@
+public class wrrap {
+    public static void main(String[] args) {
+        String st="jai shree ram";
+        String st1=String.valueOf(st);
+        String st2=st;
+        System.out.println(st2);
+        
+    }
+    
+}
